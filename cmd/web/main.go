cdc37c4eb6cf52adbd6527ef19875598f8603198
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-12-01 20:02:23 +0330

    Enable session

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/esmaeilmirzaee/bookings/pkg/config"
 	"github.com/esmaeilmirzaee/bookings/pkg/renders"
 )
@@ -11,9 +13,18 @@ import (
 const portNumber = ":8080"
 
 var app config.AppConfig
+var session *scs.SessionManager
 
 func main() {
+	// Alter the following to true in production model
 	app.IsProduction = false
+
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Persist = false
+	session.Cookie.Secure = app.IsProduction
+
+	app.Session = session
 
 	app.UseCache = false
 	tc, err := renders.CreateTemplateCache()
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -3,9 +3,26 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/esmaeilmirzaee/bookings/pkg/config"
 	"github.com/esmaeilmirzaee/bookings/pkg/models"
 	"github.com/esmaeilmirzaee/bookings/pkg/renders"
 )
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+var Repo *Repository
+
+func NewRepository(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+func NewHandlers(r *Repository) {
+	Repo = r
+}
 
 func HomePageHandler(w http.ResponseWriter, r *http.Request) {
 	renders.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
